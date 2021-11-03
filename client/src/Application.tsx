9f59881db86eeef06ab10d5276392c8cf93a6e18
Felix Anducho <felix.anducho74@gmail.com>
2021-11-02 19:27:22 -0600

    Add react-router-domn with types. Add basic folder structure with some example files

---
+++ client/src/Application.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePage from "./pages/Home";
+
+const Application: React.FunctionComponent<{}> = (props) => {
+  return <HomePage name={"HomePage"}></HomePage>;
+};
+
+export default Application;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import IPage from "../interfaces/Page";
+import logging from "../config/logging";
+
+const HomePage: React.FunctionComponent<IPage> = (props) => {
+  useEffect(() => {
+    logging.info(`Loading ${props.name}`);
+  }, [props.name]);
+  return <p>This is the home page</p>;
+};
+
+export default HomePage;
